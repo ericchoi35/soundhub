90c5e21b63b5c228dc67d29de63d05be3c9da132
Eduardo <edubaik@gmail.com>
2014-09-29 13:27:26 -0700

    Fixed indentations

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,20 +5,20 @@ var mongoose = require('mongoose')
 // Bootstrap db connection
 // Connect to mongodb
 var connect = function () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } }
-  mongoose.connect(config.db, options)
+    var options = { server: { socketOptions: { keepAlive: 1 } } }
+    mongoose.connect(config.db, options)
 }
 connect();
 // Error handler
 mongoose.connection.on('error', function (err) {
-  console.log(err)
+    console.log(err)
 })
 // Reconnect when closed
 mongoose.connection.on('disconnected', function () {
-  connect();
+    connect();
 })
 // Bootstrap models
 var models_path = __dirname + '/../server/models'
-fs.readdirSync(models_path).forEach(function (file) {
-  if (~file.indexOf('.js')) require(models_path + '/' + file)
+fs.readdirSync(models_path).forEach(function (file){
+    if (~file.indexOf('.js')) require(models_path + '/' + file)
 })
